707e3d0746b755236c1f9e0d4542b69f178d30f8
Chris Dehner <topherdehner@gmail.com>
2020-08-26 23:31:17 -0400

    Chore: Update files for video 1.8 "Namespaces"

--- c#-basics/Program.cs
+++ c#-basics/Program.cs
@@ -2,20 +2,13 @@ class Program
 {
     static void Main(string[] args)
     {
-	    System.Console.WriteLine(1.GetType());
-	    System.Console.WriteLine("hello".GetType());
-	    System.Console.WriteLine('h'.GetType());
-	    System.Int32 number = 10;
-	    System.String greeting = "Hello. How are you?";
-	    System.Console.WriteLine(number + 548);
-	    System.Console.WriteLine(greeting + " (I hope they feel well...! Whoops, there's concatenation again showing my thoughts!!)");
-	    System.Double keysmash = 3231298376.1209587;
-	    System.Console.WriteLine(keysmash.GetType());
-	    
-	    int number2 = 2;
-	    double pi = 3.14;
-	    bool what = true;
-	    string farewell = "seeya";
-	    char first = 'a';
+        // Create a new random number generator.
+        System.Random generator = new System.Random();
+        // Generate a floating-point number between 0 and 1.
+        double randomNumber = generator.NextDouble();
+        // Round the number to a single decimal place.
+        double roundedNumber = System.Math.Round(randomNumber, 1);
+        // Print the rounded number.
+        System.Console.WriteLine(roundedNumber);
     }
 }
