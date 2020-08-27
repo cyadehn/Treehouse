3b96496f75c8f9c6db54b3149f11aed9f93306cb
Chris Dehner <topherdehner@gmail.com>
2020-08-26 23:28:48 -0400

    Test C# type keywords

--- c#-basics/Program.cs
+++ c#-basics/Program.cs
@@ -9,5 +9,13 @@ class Program
 	    System.String greeting = "Hello. How are you?";
 	    System.Console.WriteLine(number + 548);
 	    System.Console.WriteLine(greeting + " (I hope they feel well...! Whoops, there's concatenation again showing my thoughts!!)");
+	    System.Double keysmash = 3231298376.1209587;
+	    System.Console.WriteLine(keysmash.GetType());
+	    
+	    int number2 = 2;
+	    double pi = 3.14;
+	    bool what = true;
+	    string farewell = "seeya";
+	    char first = 'a';
     }
 }
