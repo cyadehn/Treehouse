6537523ec26142252560d3ca5e635a25d41673ec
Chris Dehner <topherdehner@gmail.com>
2020-08-26 23:41:57 -0400

    Practice "using" declarations

--- c#-basics/Program.cs
+++ c#-basics/Program.cs
@@ -1,14 +1,16 @@
+using System;
+
 class Program
 {
     static void Main(string[] args)
     {
         // Create a new random number generator.
-        System.Random generator = new System.Random();
+        Random generator = new Random();
         // Generate a floating-point number between 0 and 1.
         double randomNumber = generator.NextDouble();
         // Round the number to a single decimal place.
-        double roundedNumber = System.Math.Round(randomNumber, 1);
+        double roundedNumber = Math.Round(randomNumber, 1);
         // Print the rounded number.
-        System.Console.WriteLine(roundedNumber);
+        Console.WriteLine(roundedNumber);
     }
 }
